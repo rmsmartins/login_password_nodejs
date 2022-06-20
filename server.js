6d5fc8ec1f19ef5c68111ddbbb9e62276b4a0ee4
rmsmartins <52045144+rmsmartins@users.noreply.github.com>
2022-06-20 18:08:01 +0100

    Criação formulãrios login.ejs, register.ejs

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 
+app.set('view-engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', { name: 'Rui' })
 });
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+});
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+});
+
+app.post('/register', (req, res) => {
+    
+});
+
 
 app.listen(3000)
